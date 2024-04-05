4a54e249c00021caa1ddf0d80a8104bb851ffd7c
CoaPro <srbpro18@gmail.com>
2024-04-05 10:12:43 +0200

    v2

--- kod.js
+++ kod.js
@@ -800,19 +800,19 @@ function RSstatistika(){
                 label: 'Vrednost',
                 data: logRSsortiraniNiz,//nizS, //logRSsortiraniNiz, //logRSsortiraniNiz, //ceoNizLogRS
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
+                    //'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 1)',
+                    //'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
+                    //'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
+                    //'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
+                    //'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
+                    //'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
@@ -848,26 +848,26 @@ function vizuelizacijaSvihPodataka(){
     vizGraf = vizGrafik.getContext('2d');
     
     vizuelizacijaPodataka = new Chart(vizGraf, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: ceoNizLogn, //ceoNizLogn, testAniz
             datasets: [{
                 label: 'Vrednost',
                 data: ceoNizLogRS, //ceoNizLogRS
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
+                    //'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 0.2)', 
                     'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
+                    //'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
+                    // 'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
+                    // 'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
+                    //'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
